ea1c7df44e3a63e918af5f0faa35160622db68a2
Ethan Zhang <ezhang@rawideas.com>
2018-06-27 18:17:02 +1000

    test

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
+
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Slider } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import { RNCamera } from 'react-native-camera';
+import  Canvas  from 'react-native-canvas';
+import RNFS from 'react-native-fs';
+import RNFetchBlob from 'rn-fetch-blob';
 
 const landmarkSize = 2;
+const base64_image_header = 'data:image/jpeg;base64';
 
 const flashModeOrder = {
   off: 'on',
@@ -34,6 +39,11 @@ export default class CameraScreen extends React.Component {
     showGallery: false,
     photos: [],
     faces: [],
+    cameraPreviewWidth: 0,
+    cameraPreviewHeight: 0,
+    cameraPreviewOffsetX: 0,
+    cameraPreviewOffsetY: 0,
+    canvasCtx: 0
   };
 
   getRatios = async function() {
@@ -96,39 +106,71 @@ export default class CameraScreen extends React.Component {
   }
 
   takePicture = async function() {
-    if (this.camera) {
-      this.camera.takePictureAsync().then(data => {
-        console.log('data: ', data);
-      });
+    if (this.camera && this.state.canvas) {
+      // this.camera.takePictureAsync().then(data => {
+      //   console.log('data: ', data);
+      // });
+
+      this.state.canvas.toDataURL('image/jpeg')
+        .then((data) => {
+          // By the way here data is wrapped in quotes by some reason, so we need to remove them
+          data = data.substring(1);
+          data = data.slice(0, -1);
+
+          if (data.indexOf(base64_image_header) > -1) {
+            // Removing "data:image/jpeg;base64," for saving into file as base64 data
+            data = data.substring(base64_image_header.length);
+          }
+
+          const imagePath = RNFS.DocumentDirectoryPath+'/image.jpg';
+
+          RNFetchBlob.fs.writeFile(imagePath, data, 'base64')
+            .then(
+              Image.getSize('file://'+imagePath, (width, height) => {
+                console.log('saved image size: ' + width, height);
+              })
+            );
+        });
+
+      //this.setState({ canvasImgUrl: imageData, showGallery: true });
     }
   };
 
   onFacesDetected = ({ faces }) => this.setState({ faces });
   onFaceDetectionError = state => console.warn('Faces detection error:', state);
 
-  renderFace({ bounds, faceID, rollAngle, yawAngle }) {
-    return (
-      <View
-        key={faceID}
-        transform={[
-          { perspective: 600 },
-          { rotateZ: `${rollAngle.toFixed(0)}deg` },
-          { rotateY: `${yawAngle.toFixed(0)}deg` },
-        ]}
-        style={[
-          styles.face,
-          {
-            ...bounds.size,
-            left: bounds.origin.x,
-            top: bounds.origin.y,
-          },
-        ]}
-      >
-        <Text style={styles.faceText}>ID: {faceID}</Text>
-        <Text style={styles.faceText}>rollAngle: {rollAngle.toFixed(0)}</Text>
-        <Text style={styles.faceText}>yawAngle: {yawAngle.toFixed(0)}</Text>
-      </View>
-    );
+  renderFace(face, ctx) {
+
+    const  { bounds, faceID, rollAngle, yawAngle } = face;
+    // return (
+    //   <View
+    //     key={faceID}
+    //     transform={[
+    //       { perspective: 600 },
+    //       { rotateZ: `${rollAngle.toFixed(0)}deg` },
+    //       { rotateY: `${yawAngle.toFixed(0)}deg` },
+    //     ]}
+    //     style={[
+    //       styles.face,
+    //       {
+    //         ...bounds.size,
+    //         left: bounds.origin.x,
+    //         top: bounds.origin.y,
+    //       },
+    //     ]}
+    //   >
+    //     <Text style={styles.faceText}>ID: {faceID}</Text>
+    //     <Text style={styles.faceText}>rollAngle: {rollAngle.toFixed(0)}</Text>
+    //     <Text style={styles.faceText}>yawAngle: {yawAngle.toFixed(0)}</Text>
+    //   </View>
+    // );
+    console.log(ctx);
+
+    const x = this.state.cameraPreviewOffsetX;
+    const y = this.state.cameraPreviewOffsetY;
+
+    ctx.strokeStyle = 'green';
+    ctx.strokeRect((bounds.origin.x-x), (bounds.origin.y-y), bounds.size.width, bounds.size.height);
   }
 
   renderLandmarksOfFace(face) {
@@ -162,11 +204,24 @@ export default class CameraScreen extends React.Component {
   }
 
   renderFaces() {
-    return (
-      <View style={styles.facesContainer} pointerEvents="none">
-        {this.state.faces.map(this.renderFace)}
-      </View>
-    );
+    if(this.canvas) {
+      if(this.state.canvasCtx === 0) {
+        this.canvas.width = this.state.cameraPreviewWidth;
+        this.canvas.height = this.state.cameraPreviewHeight;
+
+        this.setState({canvasCtx: this.canvas.getContext('2d'), canvas: this.canvas});
+      }
+      // ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      // console.log(this.canvas.width, this.canvas.height);
+
+      var ctx = this.state.canvasCtx;
+
+      return (
+        <View style={styles.facesContainer} pointerEvents="none">
+          {this.state.faces.map((face) => this.renderFace(face, ctx))}
+        </View>
+      );
+    }
   }
 
   renderLandmarks() {
@@ -177,14 +232,54 @@ export default class CameraScreen extends React.Component {
     );
   }
 
+  updatePreviewWindowSize(event) {
+    if(event) {
+      const {x, y, width, height} = event.nativeEvent.layout;
+
+      console.log(event.nativeEvent.layout);
+
+      this.setState({
+        cameraPreviewWidth: width,
+        cameraPreviewHeight: height,
+        cameraPreviewOffsetX: x,
+        cameraPreviewOffsetY: y,
+      });
+
+    }
+  }
+
   renderCamera() {
     return (
+      <View style={{
+        flex: 1,
+        flexDirection: 'column'
+      }}>
+        <View
+          style={{
+            flex: 0.1,
+            flexDirection: 'row',
+            height: 40,
+            justifyContent: 'space-around',
+          }}
+        >
+          <TouchableOpacity style={styles.flipButton} onPress={this.toggleFacing.bind(this)}>
+            <Text style={styles.flipText}> FLIP </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.flipButton} onPress={this.toggleFlash.bind(this)}>
+            <Text style={styles.flipText}> FLASH: {this.state.flash} </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.flipButton} onPress={this.toggleWB.bind(this)}>
+            <Text style={styles.flipText}> WB: {this.state.whiteBalance} </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{flex: 0.8,}}
+              onLayout={(event)=>this.updatePreviewWindowSize(event)}>
       <RNCamera
         ref={ref => {
           this.camera = ref;
         }}
         style={{
-          flex: 1,
+          flex: 1
         }}
         type={this.state.type}
         flashMode={this.state.flash}
@@ -199,43 +294,21 @@ export default class CameraScreen extends React.Component {
         permissionDialogTitle={'Permission to use camera'}
         permissionDialogMessage={'We need your permission to use your camera phone'}
       >
-        <View
-          style={{
-            flex: 0.5,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-            justifyContent: 'space-around',
+        <Canvas
+          ref={ref => {
+            this.canvas = ref;
           }}
-        >
-          <TouchableOpacity style={styles.flipButton} onPress={this.toggleFacing.bind(this)}>
-            <Text style={styles.flipText}> FLIP </Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.flipButton} onPress={this.toggleFlash.bind(this)}>
-            <Text style={styles.flipText}> FLASH: {this.state.flash} </Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.flipButton} onPress={this.toggleWB.bind(this)}>
-            <Text style={styles.flipText}> WB: {this.state.whiteBalance} </Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            flex: 0.4,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-            alignSelf: 'flex-end',
-          }}
-        >
-          <Slider
-            style={{ width: 150, marginTop: 15, alignSelf: 'flex-end' }}
-            onValueChange={this.setFocusDepth.bind(this)}
-            step={0.1}
-            disabled={this.state.autoFocus === 'on'}
-          />
+          style={{flex:1}}
+        />
+        {this.renderFaces()}
+
+      </RNCamera>
+
         </View>
         <View
           style={{
             flex: 0.1,
-            backgroundColor: 'transparent',
+            height: 20,
             flexDirection: 'row',
             alignSelf: 'flex-end',
           }}
@@ -271,14 +344,27 @@ export default class CameraScreen extends React.Component {
             <Text style={styles.flipText}> Gallery </Text>
           </TouchableOpacity>
         </View>
-        {this.renderFaces()}
-        {this.renderLandmarks()}
-      </RNCamera>
+      </View>
     );
   }
 
   render() {
-    return <View style={styles.container}>{this.renderCamera()}</View>;
+    if(this.state.showGallery) {
+      return (
+        <View style={{flex:1}}>
+          <Image
+            source={{ uri: 'file://'+this.state.canvasImgUrl+'1' }}
+            style={{
+              height: 600,
+              width: 400
+            }}
+          />
+        </View>
+      )
+    }else{
+      return <View style={styles.container}>{this.renderCamera()}</View>
+    }
+
   }
 }
 
